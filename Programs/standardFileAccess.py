72730b7e4c09cbdcc4571ee8067c5a5dfd8c3a03
khalidativeTECH <khalid@khalidative.tech>
2020-02-02 12:18:05 +0530

    update

--- Programs/standardFileAccess.py
+++ Programs/standardFileAccess.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 28 21:00:49 2020
-
-@author: khalid
-"""
-
-aString = raw_input('Enter a string: ')
-print(aString)
+def safer_float(object):
+    try:
+        return_value = float(object)  # return the object without changes
+    except ValueError:
+        return_value = "could not convert to float"
+    except TypeError:
+        return_value = "This type of cant be converted to a float"
+    return return_value
