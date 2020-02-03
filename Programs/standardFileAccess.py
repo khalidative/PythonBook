28f4b3c9936a3e978a1edf6f1ea52175af89e46b
khalidativeTECH <khalid@khalidative.tech>
2020-02-03 22:44:34 +0530

    till exception handling

--- Programs/standardFileAccess.py
+++ Programs/standardFileAccess.py
@@ -1,8 +1,4 @@
-def safer_float(object):
-    try:
-        return_value = float(object)  
-    except (ValueError, TypeError) :
-        return_value = "The argument must be a numeric string or a number"
-    else:
-        print("There were no exceptions raised!")
-    return return_value
+try:
+    assert (2 + 2) == 5
+except AssertionError:
+    print("2 + 2 is not 5")
